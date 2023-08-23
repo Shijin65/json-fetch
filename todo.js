c1e4d84ad9a1ff38545f72ff37edee9067b1fce5
shijin65 <shijincht65@gmail.com>
2023-08-23 13:09:43 +0530

    data fetched successfulley from the json place holder

--- todo.js
+++ todo.js
@@ -1,14 +1,14 @@
 
 let namel='';
-
-fetch('user.json').then(Response => Response.json()).then(JSON => {
-            console.log(JSON.user)
-            JSON.user.forEach(user => {
-                namel +=`<p> ${user.name} - ${user.age}</p>  `
-                console.log(user.name)
+let todo=''
+fetch('https://jsonplaceholder.typicode.com/todos').then(Response => Response.json()).then(JSON => {
+            console.log(JSON[1].title)
+            JSON.forEach(todo => {
+                namel +=`<p> ${todo.title}</p>  `
+                console.log(todo.title)
             }); 
 
-            
+
            document.querySelector('.todo').innerHTML=namel;
            
 
